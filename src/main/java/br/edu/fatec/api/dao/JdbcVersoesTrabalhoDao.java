2f5ba0d88083423b84fa8bef1d9d594ae7280959
Flavio Pereira Jr <flavjuni98@gmail.com>
2025-11-12 21:34:17 -0300

    feat<SCRUM-116> Foi adicionado a tela de comparar de versões de tg

--- src/main/java/br/edu/fatec/api/dao/JdbcVersoesTrabalhoDao.java
+++ src/main/java/br/edu/fatec/api/dao/JdbcVersoesTrabalhoDao.java
@@ -7,6 +7,7 @@ import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class JdbcVersoesTrabalhoDao {
     
@@ -77,5 +78,45 @@ public class JdbcVersoesTrabalhoDao {
         }
         
         throw new SQLException("Trabalho não encontrado para o aluno " + alunoId);
+    }
+
+    public Optional<VersaoHistoricoDTO> findUltimaVersaoCorrigida(Long trabalhoId) throws SQLException {
+        final String sql = """
+            SELECT
+                vt.id, vt.versao, vt.conteudo_md, vt.comentario, vt.created_at
+            FROM
+                versoes_trabalho AS vt
+            WHERE
+                vt.trabalho_id = ? AND vt.secao = 'COMPLETO'
+                AND EXISTS (
+                    SELECT 1
+                    FROM pareceres p
+                    WHERE p.trabalho_id = vt.trabalho_id
+                      AND p.versao = vt.versao
+                )
+            ORDER BY
+                vt.created_at DESC
+            LIMIT 1
+        """;
+
+        try (Connection con = Database.get();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setLong(1, trabalhoId);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return Optional.of(new VersaoHistoricoDTO(
+                            rs.getLong("id"),
+                            rs.getString("versao"),
+                            rs.getString("conteudo_md"),
+                            // Este é o 'versoes_trabalho.comentario', que ainda pode ser nulo (e tudo bem)
+                            rs.getString("comentario"),
+                            rs.getTimestamp("created_at").toLocalDateTime()
+                    ));
+                }
+            }
+        }
+        return Optional.empty(); // Nenhuma versão com pareceres encontrada
     }
 }
